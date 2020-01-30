6fe764ecd234eaf19a053a43dbdf1cfcb44e8853
tuzigit <767405711@qq.com>
2020-01-30 15:06:07 +0800

    添加pow类

---
+++ charpter1/Pow.java
@@ -0,0 +1,21 @@
+package charpter1;
+/**
+ * Pow 求幂2
+ */
+
+class Pow{
+    public static long pow(long x, int n){
+        if (n==0){
+            return 1;
+        }else if (n==1){
+            return x;
+        }else if (n %2 ==0){
+            return pow(x*x,n/2);
+        }else {
+            return x*pow(x*x,(n-1)/2);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(pow(4,3));
+    }
+}
