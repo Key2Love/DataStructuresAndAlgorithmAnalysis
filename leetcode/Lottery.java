7bb003c3cfd45c099139c4d39d56d7b4a9058939
tuzigit <767405711@qq.com>
2020-03-25 20:34:53 +0800

    xxx

---
+++ leetcode/Lottery.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * @program: algorithm
+ * @description:
+ * @author: Sunbuhui7
+ * @create: 2020-03-24 20:43
+ **/
+
+public class Lottery {
+}
---
+++ leetcode/threeDarea.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * @program: algorithm
+ * @description:
+ * @author: Sunbuhui7
+ * @create: 2020-03-25 09:59
+ **/
+
+public class threeDarea {
+}
